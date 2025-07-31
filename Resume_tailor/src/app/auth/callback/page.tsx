b4db946f77c9f0dd9d93546b4a111729236bb24a
Alishba Hassan <hassan.alishba20@gmail.com>
2025-07-31 13:56:40 +0500

    Update page.tsx

--- Resume_tailor/src/app/auth/callback/page.tsx
+++ Resume_tailor/src/app/auth/callback/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { createClientComponentClient } from  "@supabase/ssr";
+import { createBrowserClient } from  "@supabase/ssr";
 
 export default function AuthCallback() {
   const router = useRouter();
