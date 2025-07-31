98db4f73c360039b6a7c108ddd8a132c4140b981
Alishba Hassan <hassan.alishba20@gmail.com>
2025-07-31 15:19:04 +0500

    Update page.tsx

--- Resume_tailor/src/app/auth/callback/page.tsx
+++ Resume_tailor/src/app/auth/callback/page.tsx
@@ -3,12 +3,13 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { createBrowserClient } from "@supabase/ssr";
+
 export default function AuthCallback() {
   const router = useRouter();
   const supabase = createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-    );  
+  );  
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
